272c2a62837a220d1604ced2edfdc21364a43e11
s172084 <s172084@dtu.dk>
2023-08-14 00:45:20 +0200

    Up to Logistic Regression - - Sleep - Continue Tmro.

--- Ansogning_Code.R
+++ Ansogning_Code.R
@@ -22,21 +22,43 @@ gravier_clean
 View(gravier_clean)
 
 # From clean data, transform to long format. 
+gravier_long <- pivot_longer( data = gravier_clean,
+                                   cols = ! starts_with("o"),
+                                   names_to = "gene")
+
+
+gravier_long
 
 # Choose 100 random genes. 
+gravier_long_s <- sample_n(gravier_long,100)
+gravier_long_s 
+
+gravier_long_sren <- rename(gravier_long_s ,log2_expr_level = value)
+gravier_long_sren
+
 
 # Fit a logistic regression to each gene, modelling: “outcome ~ log2_expr_lvl”
+# *** Its correct until here... (Sleep)***
+
+gravier_data_nested <-gravier_data_nested %>%
+  mutate(model = map(data, ~ glm(outcome ~ log2_expr_level,
+                                 gravier_data_nested
+                                 data = .,
+                                 family = binomial(link = "logit"))))
+
+gravier_data_nested <- gravier_data_nested %>%
+  # for each model, generate tidy data with estimate, std.error, statistic, p-value
+  mutate(tidied_model = map(model, tidy, conf.int = TRUE)) %>% # take the tidied model out of a tibble and show it.
+  unnest(tidied_model)
+gravier_data_nested
+
+
+gravier_data_long_nested <- gravier_data_nested %>%
+  filter(str_detect(term,"log2_expr_level"))
+gravier_data_long_nested
 
 # Add beta-estimates and confidence intervals
-
 # Add an indicator for whether the p-value was less than or equal to 0.05
-
 # Add code to the README showing short analysis with nice clear code.
-
 # That is your long modelled data,create a forest-plot of the slopes (beta1 estimates) 
 # and add 95% CI
-
-# Write data
-write_tsv(gravier_clean, file = "data/01_gravier_clean.tsv.gz")
-
-
